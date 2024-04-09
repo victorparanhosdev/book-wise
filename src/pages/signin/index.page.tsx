90872eddcaf1dd7e43a122a43da8acf75f8e7cf4
Victor Paranhos <victor_paranhos@hotmail.com>
2024-04-09 12:03:25 -0300

    login

--- src/pages/signin/index.page.tsx
+++ src/pages/signin/index.page.tsx
@@ -1,12 +1,31 @@
-import { Container, Login } from "./styles"
+import { Container, BoxLogin, Login, Button } from "./styles"
 import Image from "next/image"
-
+import googleLogo from '@/src/assets/logos_google-icon.png'
+import gitLogo from '@/src/assets/Clip path group.png'
+import rocketLogo from '@/src/assets/RocketLaunch.png'
 import BackGround from '@/src/assets/backgroundlogin.png'
 export default function SignIn(){
     return(
         <Container>
+            <section>
             <Image src={BackGround} height={912} width={598} alt="Background Login"/>
-            <Login>Caixa de login</Login>
+            </section>
+            
+            <section>
+                <BoxLogin>
+                    <h1>Boas vindas!</h1>
+                    <p>Faça seu login ou acesse como visitante</p>
+
+                <Login><Button><Image src={googleLogo} height={32} width={32}   alt="Google"/>Entrar com Google</Button>
+                    <Button><Image src={gitLogo} height={32} width={32} alt="Github"/>Entrar com GitHub</Button>
+                    <Button><Image src={rocketLogo} height={32} width={32} alt="Visitante"/>Acessar como visitante</Button>
+                </Login>
+                 
+                
+                
+                </BoxLogin>
+            </section>
+
         </Container>
     )
 }
--- src/pages/signin/styles.ts
+++ src/pages/signin/styles.ts
@@ -3,9 +3,8 @@ import { styled } from "@/styles/stitches.config"
 export const Container = styled('main', {
     height: 'calc(100vh - 4rem)',
     margin: '2rem',
-    color: 'White',
     display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
+    gridTemplateColumns: '59.8rem 1fr',
 
     img:{
         objectFit: 'cover',
@@ -13,16 +12,58 @@ export const Container = styled('main', {
         width: '100%',
         borderRadius: '1rem'
 
+    },
+
+    'section:last-child': {
+        height: 'calc(100vh - 4rem)',
+        width: '100%',
+        display: 'grid',
+        placeContent: 'center',
     }
+
+
 })
 
-export const Login = styled('div', {
+export const BoxLogin = styled('div', {
+    width: '37.2rem',
     h1: {
         color: '$gray100',
         fontSize: '$heading_lg',
     },
     p: {
         color: '$gray200',
-        fontSize: '$text_md'
+        fontSize: '$text_md',
+        marginBottom: '4rem'
     }
+})
+
+export const Button = styled('button', {
+    all: 'unset',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    gap:'$20',
+    color: '$gray200',
+    fontSize: '$button_lg',
+    background: '$gray600',
+    borderRadius: '$sm',
+    padding: '2rem 2.4rem',
+
+
+    '&:hover': {
+        transition: 'background 0.2s',
+        background: '$gray500'
+    },
+
+    img: {
+        width: '3.2rem',
+        objectFit: 'cover'
+    }
+ 
+    
+})
+
+export const Login = styled('div', {
+    display: 'grid',
+    gap: '1.6rem'
 })
