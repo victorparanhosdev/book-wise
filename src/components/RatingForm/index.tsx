039f99bee75ee7b00f2f17ec845dc2251ea25848
Victor Paranhos <victor_paranhos@hotmail.com>
2024-05-09 16:18:33 -0300

    rate

--- src/components/RatingForm/index.tsx
+++ src/components/RatingForm/index.tsx
@@ -1,23 +1,43 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Avatar } from "../Avatar";
 import { RatingStart } from "../RatingStart";
 import { Container, UserDetails, ContainerButton } from "./styles";
 import { useSession } from "next-auth/react";
 import { X, Check } from "phosphor-react";
 import { TextArea } from "../TextArea";
+import { useMutation } from "@tanstack/react-query";
+import { api } from "@/src/lib/axios";
 
-type RatingFromProps = {
+
+
+
+type RatingFromProps ={
   bookId: string;
   onCancel: () => void;
 };
 
-export const RatingForm = ({ bookId, onCancel }: RatingFromProps) => {
+export const RatingForm = ({ bookId, onCancel, ...props }: RatingFromProps) => {
   const { data } = useSession();
   const [currentRating, setCurrentRating] = useState(0);
   const [description, setDescription] = useState("");
   const user = data?.user;
 
-  const disabledDescription = !description.trim() || !currentRating
+  const submitDisabled = !description.trim() || !currentRating
+
+  const {mutateAsync: handleRate} = useMutation({mutationFn: async () => {
+    await api.post(`/books/${bookId}/rate`, {
+      description,
+      rate: currentRating,
+
+    })
+  }})
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    if(submitDisabled) return
+    await handleRate()
+
+  }
 
   return (
     <Container>
@@ -31,7 +51,7 @@ export const RatingForm = ({ bookId, onCancel }: RatingFromProps) => {
 
             <RatingStart setRating={setCurrentRating} size={24} valueRating={currentRating} />
           </UserDetails>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
             <TextArea
             maxLength={450}
             value={description}
@@ -43,7 +63,7 @@ export const RatingForm = ({ bookId, onCancel }: RatingFromProps) => {
             <button onClick={onCancel} type="button">
               <X style={{ color: "#8381D9" }} size={24} />
             </button>
-            <button disabled={disabledDescription} type="submit">
+            <button disabled={submitDisabled} type="submit">
               <Check size={24} />
             </button>
           </ContainerButton>
